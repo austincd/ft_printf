fc77ad9e488eca2e058f427da1e82cdde3c4857c
Austin Daly <austincd@protonmail.com>
2017-05-27 01:00:07 -0700

    auto

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 06:37:13 by adaly             #+#    #+#             */
-/*   Updated: 2017/05/27 00:56:19 by adaly            ###   ########.fr       */
+/*   Updated: 2017/05/27 01:00:00 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_printf(char *str, ...)
 	char				*new;
 	int					after;
 
-//	printf("%s\n", str);
+	printf("%s\n\n\n\n", str);
 	after = 0;
 	va_start(args, str);
 	new = NULL;
