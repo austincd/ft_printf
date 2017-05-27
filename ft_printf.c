ffa22f9fbb8b8331c6cccc9689597d9675a86609
Austin Daly <austincd@protonmail.com>
2017-05-27 03:26:34 -0700

    auto

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 06:37:13 by adaly             #+#    #+#             */
-/*   Updated: 2017/05/27 03:25:41 by adaly            ###   ########.fr       */
+/*   Updated: 2017/05/27 03:26:30 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int		ft_printf(char *str, ...)
 //		printf("%s\n", conversions[index]->string);
 		chars += conversions[index]->chars - conversions[index]->conv_length;
 //	if (conversions[index]->type == 'c')
-		after += ft_find_replace_char(&new, conversions[index]->orig, conversions[index]->string[0], after);
+//		after += ft_find_replace_char(&new, conversions[index]->orig, conversions[index]->string[0], after);
 //	else
 		after += ft_find_replace_after(&new, conversions[index]->orig, conversions[index]->string, after);
 		++index;
