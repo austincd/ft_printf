6db5684fb10e3285a403a8a649d24dd0b66efa47
Austin Daly <austincd@protonmail.com>
2017-06-02 18:58:13 -0700

    added ft_e since ft_a is done

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 18:47:08 by adaly             #+#    #+#             */
-/*   Updated: 2017/06/02 18:54:22 by adaly            ###   ########.fr       */
+/*   Updated: 2017/06/02 18:58:05 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ char	*ft_ftoa(long double num)
 		ft_charcat(str, '-');
 	while (num >= 0.000000000000000001)
 	{
-		printf("num is %Lf\n", num);
-		printf("n/p is %Lf\n", num / power);
-		printf("lln/p is %lld\n", (long long)((num / power) + .5));
-		ft_charcat(str, digits[(long long)((num / power))]);
+//		printf("num is %Lf\n", num);
+//		printf("n/p is %Lf\n", num / power);
+//		printf("lln/p is %lld\n", (long long)((num / power) + .5));
+		ft_charcat(str, digits[(long long)((num / power) + .5)]);
 //		if (base == 10 && num - (num / power) < 0.00000000000001)
 //			num = 0;
 //		else
