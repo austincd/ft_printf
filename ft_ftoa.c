ff5bc732944eb50b6fcd04915c36e10cf67636bf
Austin Daly <austincd@protonmail.com>
2017-06-02 19:00:10 -0700

    added ft_e since ft_a is done

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 18:47:08 by adaly             #+#    #+#             */
-/*   Updated: 2017/06/02 18:58:05 by adaly            ###   ########.fr       */
+/*   Updated: 2017/06/02 19:00:01 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*ft_ftoa(long double num)
 	str = ft_strnew(counter + 9);
 	if (sign == -1)
 		ft_charcat(str, '-');
-	while (num >= 0.000000000000000001)
+	while (num >= 0.000000000000000001 || counter > -10)
 	{
 //		printf("num is %Lf\n", num);
 //		printf("n/p is %Lf\n", num / power);
