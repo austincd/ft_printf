e6b8cbeb8cfcb6b18534ef01053c2645488c0e54
Austin Daly <austincd@protonmail.com>
2017-06-04 03:16:57 -0700

    pushing

--- ft_floating_types.c
+++ ft_floating_types.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/30 14:21:01 by adaly             #+#    #+#             */
-/*   Updated: 2017/06/04 03:15:09 by adaly            ###   ########.fr       */
+/*   Updated: 2017/06/04 03:16:52 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ static void		ft_g(t_pfconv *current, long double num)
 	length1 = ft_strlen(current->string);
 	printf("normal is %s\n", temp);
 	printf("standard is %s%s\n", current->string, current->exponent);
-	if (length1 + ft_strlen(current->exponent) < length2)
+	if (length1 + ft_strlen(current->exponent) < length2 || length2 > 6)
 		free(temp);
 	else
 	{
