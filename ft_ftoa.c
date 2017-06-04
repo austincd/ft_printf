daf75fd71c06699381b04523ff21ceff96a1f765
Austin Daly <austincd@protonmail.com>
2017-06-04 02:09:12 -0700

    ft_ftoa.c

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 18:47:08 by adaly             #+#    #+#             */
-/*   Updated: 2017/06/02 19:03:31 by adaly            ###   ########.fr       */
+/*   Updated: 2017/06/04 02:08:54 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ char	*ft_ftoa(long double num)
 //		printf("num is %Lf\n", num);
 //		printf("n/p is %Lf\n", num / power);
 //		printf("lln/p is %lld\n", (long long)((num / power) + .5));
-		ft_charcat(str, digits[(long long)((num / power) + .5)]);
+		ft_charcat(str, digits[(long long)((num / power) + .1)]);
 //		if (base == 10 && num - (num / power) < 0.00000000000001)
 //			num = 0;
 //		else
