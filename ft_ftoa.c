ec2178f2217128cfe2a4137d5a2afe2f01de4322
Austin Daly <austincd@protonmail.com>
2017-06-04 02:22:46 -0700

    pushing

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: adaly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 18:47:08 by adaly             #+#    #+#             */
-/*   Updated: 2017/06/04 02:21:30 by adaly            ###   ########.fr       */
+/*   Updated: 2017/06/04 02:22:39 by adaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ char	*ft_ftoa(long double num)
 		ft_charcat(str, '-');
 	while (num >= 0.000000000000000001 || counter > -7)
 	{
-//		printf("num is %Lf\n", num);
-//		printf("n/p is %Lf\n", num / power);
-//		printf("lln/p is %lld\n", (long long)((num / power) + .5));
-		ft_charcat(str, digits[(long long)((num / power) + .2)]);
+		printf("num is %Lf\n", num);
+		printf("n/p is %Lf\n", num / power);
+		printf("lln/p is %lld\n", (long long)((num / power) + .5));
+		ft_charcat(str, digits[(long long)((num / power) + .1)]);
 //		if (base == 10 && num - (num / power) < 0.00000000000001)
 //			num = 0;
 //		else
